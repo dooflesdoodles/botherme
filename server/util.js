b1039d724172d4eafaa657afdec9cdad3d4afd82
Erik Geiser <erik.geiser@posteo.net>
2018-07-11 21:37:16 +0200

    Clean up utils and package.json

--- server/util.js
+++ server/util.js
@@ -2,14 +2,12 @@
  *  Helpers
  */
 
-const path = require('path');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const config = require('../webpack.development.config.js');
 
 
 module.exports = {
-  staticFile: filePath => path.join(__dirname, '..', 'client', 'static', filePath || ''),
   loadWebpackDevMiddleware: (app) => {
     console.log('Setting up webpack development middleware');
 
